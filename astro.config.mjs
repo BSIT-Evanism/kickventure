5ab614596fd0f2a1059531dea1f7ccbe45563a11
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-12-08 04:18:11 +0800

    hf4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
-
+import vercel from "@astrojs/vercel/serverless";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sentry(), spotlightjs()],
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone"
+  })
 });
